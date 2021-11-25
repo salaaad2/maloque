76f4f5797088157b4d928e2040f2976e7cb9c452
salaaad2 <arthurdurant263@gmail.com>
2021-11-25 15:57:18 +0100

    j'ai faim

--- src/ft_malloc.c
+++ src/ft_malloc.c
@@ -17,7 +17,7 @@
 #include <errno.h>
 
 static void *
-m_alloc(void ** mapped, uint32_t m, uint32_t * left, uint32_t size)
+m_alloc(void ** mapped, uint32_t size, uint32_t m, t_mlc * mlc)
 {
     /*
     ** if memory region is not allocated,
@@ -25,7 +25,7 @@ m_alloc(void ** mapped, uint32_t m, uint32_t * left, uint32_t size)
     */
     void * ret;
 
-    if (*left == 0) {
+    if (mlc->left == 0) {
         *mapped = mmap(NULL, m, PROT_READ|PROT_WRITE, MAP_PRIVATE|MAP_ANONYMOUS, 0, 0);
         ret = *mapped;
     } else {
@@ -39,47 +39,30 @@ m_alloc(void ** mapped, uint32_t m, uint32_t * left, uint32_t size)
     /*
     ** increase left
     */
-    *left += (size + 1);
+    mlc->left += (size + 1);
+    mlc->size = size;
     return (ret);
 }
 
 void *
 ft_malloc(uint32_t size)
 {
-    static uint32_t tiny_left = 0;
-    static uint32_t small_left = 0;
-    static uint32_t large_left = 0;
-    static void * tiny_mapped = 0;
-    static void * small_mapped = 0;
-    static void * large_mapped = 0;
+    static t_mlc mlc;
     uint32_t m;
     uint32_t * left;
     void ** mapped;
-    int cfd;
-    int off;
 
-    m = 0;
-    cfd = 0;
-    off = 0;
-    left = 0;
     /*
     ** which multiple of page size should we use ?
     */
-    if ((size + tiny_left) < PG_TINY) {
+    if (size < PG_TINY) {
         m = PG_TINY;
-        left = &tiny_left;
-        mapped = &tiny_mapped;
     } else {
-        if ((size + small_left) < PG_SMALL) {
+        if (size < PG_SMALL) {
             m = PG_SMALL;
-            left = &small_left;
-            mapped = &small_mapped;
         } else {
-            write(1, "large\n", 6);
             m = PG_LARGE;
-            left = &large_left;
-            mapped = &large_mapped;
         }
     }
-    return (m_alloc(mapped, m, left, size));
+    return (m_alloc(mapped, size, m, &mlc));
 }
